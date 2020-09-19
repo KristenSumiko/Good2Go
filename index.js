e7b12f3c16153296d0c43ce921e3988c644c84e6
Kristen Sumiko Jones <kristensumiko@gmail.com>
2020-09-19 06:05:41 -0400

    basic HTML and index.js psuedo code added

---
+++ index.js
@@ -0,0 +1,56 @@
+//WRAP IN STRICT FUNCTION
+
+//function handleSubmit() {
+    //create variables for input values
+    //listens to form for when Search button is pressed {
+        //calls functions to get Flight, Hotel, and Covid data
+    //}
+//}
+
+//function getFlightData() {
+    //searches Flight API for input dates and location {
+        //updates STORE w flight data returned
+    //}
+//}
+
+//function getHotelData() {
+    //searches Hotel API for input dates {
+        //updates STORE w hotel data returned
+    //}
+//}
+
+//function getCovidData() {
+    //searches Covid API for inputted location {
+        //updates STORE w Covid data returned
+    //}
+//}
+
+//function generateFlightHTML() {
+    //loops through flight data and creates HTML
+//}
+
+//function generateHotelHTML() {
+    //loops through hotel data and creates HTML
+//}
+
+//function generateCovidData() {
+    //creates HTML for Covid data
+//}
+
+//function updateFlightData() {
+    //gets 10 (or next 10) flight options to pass to generateFlightHTML()
+//}
+
+//function updateHotelHTML() {
+    //gets 5 (or next 5) hotel options to pass to generateHotelHTML()
+//}
+
+//function updateCovidHTML() {
+    //calls generateCovidData
+//}
+
+//function handleRestart() {
+    // brings back to homepage and clears all previous inputs and STORE
+//}
+
+//READY FUNCTION
